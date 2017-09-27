2fe0fcc68b7667c9a1811886ced369f8bfb87270
sample name <sample_email@gmail.com>
2017-09-27 12:16:56 +0300

    changed the main action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
 
   def calculate
      a=params[:a].to_i
-     b=params[:b].to_i			
-     render html: a+b
+     b=params[:b].to_i	
+     c=params[:c].to_i
+
+     d=b*b-4*a*c
+     if(d<0)
+       render html: "No real solutions"
+     else 
+        x1=(-b+Math.sqrt(d))/2*a
+        x2=(-b-Math.sqrt(d))/2*a
+        render html: "x1->#{x1} x2->#{x2}"    
+     end
+         
   end
 end
