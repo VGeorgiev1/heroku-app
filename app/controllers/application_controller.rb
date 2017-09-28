975a7ba56705278322091e769a63e72c3d123728
Vladislav <vladdislav.georgiev@gmail.com>
2017-09-28 21:50:45 +0300

    added uploading csv documents and diplaying them in a table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  require 'csv'
   def calculate
-     render 'hello.erb.html'    
+     render 'hello'    
   end
+  def upload
+    file=params[:file].read
+
+    @info = CSV.parse(file)
+    
+    render 'table'
+  end  
 end
