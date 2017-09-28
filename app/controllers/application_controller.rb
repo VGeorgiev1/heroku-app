363d3c6a17c4d43ad1b6ae82788806a56637cd8f
Vladislav <vladdislav.georgiev@gmail.com>
2017-09-29 01:38:53 +0300

    chnage the way way of a summation of a column

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
   require 'csv'
+  $foo='Hello!';
   def calculate
      render 'hello'    
   end
@@ -9,11 +10,17 @@ class ApplicationController < ActionController::Base
     file=params[:file].read
 
     @info = CSV.parse(file)
-    
+    $foo=@info
     render 'table'
   end  
   def calc
-      p params
-      render html: "Hello!"
+    col=params[:selected]
+
+    index=$foo[0].index(col)
+    sum=0;
+    $foo.drop(1).each do |row|
+      sum+=row[index].to_i
+    end  
+      render html: sum
   end  
 end
