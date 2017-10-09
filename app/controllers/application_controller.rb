56251b66833c4738b27f1e92cee91889a442a530
Vladislav <vladdislav.georgiev@gmail.com>
2017-10-09 15:42:17 +0300

    i hope its the last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,18 +7,20 @@ class ApplicationController < ActionController::Base
     p params
     file=CSV.parse(params[:file].read)
     sum=0;
-    sum=file.drop(1).map{|n| n[0]}.reduce{|a, b|a.to_i+b.to_i} 
-    render plain: sum  
+    sum=file.map{|n| n[0]}.reduce{|a, b|a.to_i+b.to_i} 
+    sum=sum.ceil
+    render plain: ('%.2f'%sum)  
   end  
   def filter
     file=CSV.parse(params[:file].read)
     sum=0;    
-    file.drop(1).each do |row|
-      if row[1].to_i % 2!=0 
-        sum+=row[0].to_i
+    file.each do |row|
+      if row[2].to_i % 2!=0 
+        sum+=row[1].to_i
       end
     end 
-    render plain: sum
+    sum=sum.ceil
+    render plain: '%.2f'%sum
   end 
   def interval
     sum=0
@@ -37,17 +39,18 @@ class ApplicationController < ActionController::Base
       sum=0;
       currow+=1  
    end
-    render plain: bestInc
+   bestInc=bestInc.ceil
+    render plain: '%.2f' % bestInc
   end 
   def prediction
     file=CSV.parse(params[:file].read)
-    ind=(1..file.length-1).to_a;
-    values=file.drop(1).map{|n| n[0].to_i}
+    ind=(1..file.length).to_a;
+    values=file.map{|n| n[0].to_i}
     p values.length;
     p ind.length;
     linear = Regression::Linear.new(ind, values);
     b = linear.intercept;
     a = linear.slope;
-    render plain: "#{a},#{b}"
+    render plain: "#{'%.6f' % a},#{'%.6f' % b}"
   end  
 end
