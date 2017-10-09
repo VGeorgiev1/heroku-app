a09e1bfbad1851ae5356892298021b2ddd50b528
Vladislav <vladdislav.georgiev@gmail.com>
2017-10-09 14:42:12 +0300

    asd

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_sessions
   skip_before_action :verify_authenticity_token
   def sum
-    file=CSV.parse(params[:file].read)
+    #file=CSV.parse(params[:file].read)
     sum=0;
     sum=file.drop(1).map{|n| n[0]}.reduce{|a, b|a.to_i+b.to_i} 
-    render plain: arr  
+    render plain: sum  
   end  
   def filter
     file=CSV.parse(params[:file].read)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	post '/sum' => 'application#sum'
+	get '/sum' => 'application#sum'
 	post '/filters' => 'application#filter'
 	post '/intervals' => 'application#interval'
-	get '/lin_regressions' => 'application#prediction'
+	post '/lin_regressions' => 'application#prediction'
 end
